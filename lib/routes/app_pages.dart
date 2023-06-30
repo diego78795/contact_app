801ff59aa3c2010f3f226cefc68d962d5bb2fbd4
Diego Camargo <diego.cruz.camargo@gmail.com>
2023-06-30 12:13:31 -0300

    feat: Implementando Home Binding nas sua rota

--- lib/routes/app_pages.dart
+++ lib/routes/app_pages.dart
@@ -1,13 +1,12 @@
 import 'package:get/get.dart';
-import 'package:contact_app/ui/home_page.dart';
+import 'package:contact_app/ui/home/home_page.dart';
+import 'package:contact_app/bindings/home_binding.dart';
 
 part './app_routes.dart';
 
 abstract class AppPages {
   static final pages = [
     GetPage(
-      name: Routes.home,
-      page: () => const HomePage(),
-    )
+        name: Routes.home, page: () => const HomePage(), binding: HomeBinding())
   ];
 }
