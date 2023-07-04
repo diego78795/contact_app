e2bb144dd5037d0f583f8fa05bbf07d846fc4d17
Diego Camargo <diego.cruz.camargo@gmail.com>
2023-07-04 10:10:04 -0300

    feat: Criando função que edita contato no Contact Controller

--- lib/controller/contact_controller.dart
+++ lib/controller/contact_controller.dart
@@ -37,4 +37,23 @@ class ContactController extends GetxController {
 
     contactData = contactBox.get(Get.arguments['key'])!;
   }
+
+  Future<void> editContact(String name, Map contact) async {
+    isLoading = true;
+    update();
+    const FlutterSecureStorage secureStorage = FlutterSecureStorage();
+
+    final String? key = await secureStorage.read(key: 'key');
+    final Uint8List decodeKey = base64Url.decode(key!);
+    HiveAesCipher cipher = HiveAesCipher(decodeKey);
+
+    final Box<Map> contactBox =
+        await Hive.openBox<Map>('contactList', encryptionCipher: cipher);
+
+    await contactBox.put(Get.arguments['key'], contact);
+
+    contactData = contact;
+    isLoading = false;
+    update();
+  }
 }
