f71d59b35c034bdc042fe3b8b108258da740b2df
Diego Camargo <diego.cruz.camargo@gmail.com>
2023-07-04 09:37:48 -0300

    feat: Renderizando as informações do contato na Contact Page

--- lib/ui/contact/contact_page.dart
+++ lib/ui/contact/contact_page.dart
@@ -10,8 +10,80 @@ class ContactPage extends GetView<ContactController> {
   Widget build(BuildContext context) {
     return Scaffold(body: GetBuilder<ContactController>(builder: (_) {
       return SafeArea(
-        child: Container(),
+        child: ListView(
+          padding: const EdgeInsets.all(20),
+          children: [
+            Center(
+              child: Container(
+                width: 200,
+                height: 200,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(100),
+                  color: Colors.blue,
+                ),
+              ),
+            ),
+            const SizedBox(height: 40),
+            const Text(
+              'Informações',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+              ),
+            ),
+            const SizedBox(height: 20),
+            TextInfo(
+              label: 'Nome',
+              data: _.contactData['name'],
+            ),
+            const SizedBox(height: 10),
+            TextInfo(
+              label: 'Email',
+              data: _.contactData['email'],
+            ),
+            const SizedBox(height: 10),
+            TextInfo(
+              label: 'Telefone',
+              data: _.contactData['telephone'],
+            ),
+            const SizedBox(height: 10),
+            TextInfo(
+              label: 'Data de nascimento',
+              data: _.contactData['birthdate'] != ''
+                  ? _.contactData['birthdate']
+                  : '-',
+            )
+          ],
+        ),
       );
     }));
+  }
+}
+
+class TextInfo extends StatelessWidget {
+  const TextInfo({super.key, required this.label, required this.data});
+
+  final String? label;
+  final String? data;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Text(
+          "$label: ",
+          style: const TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 16,
+          ),
+        ),
+        Text(
+          "$data",
+          style: const TextStyle(
+            fontSize: 16,
+          ),
+        )
+      ],
+    );
   }
 }
