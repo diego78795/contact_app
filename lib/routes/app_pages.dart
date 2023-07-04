7261e92b353f3c9e341a4d8dd3db67bf68ae7ef0
Diego Camargo <diego.cruz.camargo@gmail.com>
2023-07-04 09:24:42 -0300

    fix: Corrigindo pagina da Contact Page

--- lib/routes/app_pages.dart
+++ lib/routes/app_pages.dart
@@ -2,7 +2,7 @@ import 'package:get/get.dart';
 import 'package:contact_app/ui/home/home_page.dart';
 import 'package:contact_app/bindings/home_binding.dart';
 
-import 'package:contact_app/ui/home/contact_page.dart';
+import 'package:contact_app/ui/contact/contact_page.dart';
 import 'package:contact_app/bindings/contact_binding.dart';
 
 part './app_routes.dart';
