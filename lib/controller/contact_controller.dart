de19bd5737817e15843d99b3a173fd74138d22a2
Diego Camargo <diego.cruz.camargo@gmail.com>
2023-07-04 09:37:16 -0300

    feat: Pegando informações do contato armazenado no Contact Controller

--- lib/controller/contact_controller.dart
+++ lib/controller/contact_controller.dart
@@ -1,5 +1,40 @@
+import 'dart:convert';
 import 'package:get/get.dart';
+import 'package:flutter/foundation.dart';
+
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 class ContactController extends GetxController {
   ContactController();
+
+  bool isLoading = true;
+  Map contactData = {};
+
+  @override
+  void onInit() {
+    fetchData();
+    super.onInit();
+  }
+
+  fetchData() async {
+    await initStorage();
+    isLoading = false;
+    update();
+  }
+
+  Future<void> initStorage() async {
+    await Hive.initFlutter();
+
+    const FlutterSecureStorage secureStorage = FlutterSecureStorage();
+
+    final String? key = await secureStorage.read(key: 'key');
+    final Uint8List decodeKey = base64Url.decode(key!);
+    HiveAesCipher cipher = HiveAesCipher(decodeKey);
+
+    final Box<Map> contactBox =
+        await Hive.openBox<Map>('contactList', encryptionCipher: cipher);
+
+    contactData = contactBox.get(Get.arguments['key'])!;
+  }
 }
