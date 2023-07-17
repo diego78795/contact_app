1783457eb9a12f6e8bbcf05d63c9dae340d7e8ab
Diego Camargo <diego.cruz.camargo@gmail.com>
2023-07-17 16:07:27 -0300

    feat: Adicionando repository e storage na binding do Home Controller

--- lib/bindings/home_binding.dart
+++ lib/bindings/home_binding.dart
@@ -1,10 +1,14 @@
 import 'package:get/get.dart';
 
+import 'package:contact_app/data/providers/storage.dart';
 import 'package:contact_app/controller/home_controller.dart';
+import 'package:contact_app/data/repository/contact_repository.dart';
 
 class HomeBinding implements Bindings {
   @override
   void dependencies() {
-    Get.lazyPut<HomeController>(() => HomeController());
+    Get.put(HomeController(
+      contactRepository: ContactRepository(storageClient: StorageClient()),
+    ));
   }
 }
