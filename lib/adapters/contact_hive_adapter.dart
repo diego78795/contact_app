a7ae8c09e3da5cd10bfd57c426e9e47acf9d74f9
Diego Camargo <diego.cruz.camargo@gmail.com>
2023-07-17 16:01:39 -0300

    feat: Adapter do modelo de contato para o Hive

---
+++ lib/adapters/contact_hive_adapter.dart
@@ -0,0 +1,32 @@
+import 'package:contact_app/data/model/contact_model.dart';
+
+import 'package:hive_flutter/hive_flutter.dart';
+
+class ContactHiveAdapter extends TypeAdapter<ContactModel> {
+  @override
+  final typeId = 0;
+
+  @override
+  ContactModel read(BinaryReader reader) {
+    return ContactModel(
+      name: reader.readString(),
+      nickname: reader.readString(),
+      email: reader.readString(),
+      telephone: reader.readString(),
+      gender: reader.readString(),
+      birthdate: reader.readString(),
+      image: reader.readString(),
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, ContactModel obj) {
+    writer.writeString(obj.name);
+    writer.writeString(obj.nickname);
+    writer.writeString(obj.email);
+    writer.writeString(obj.telephone);
+    writer.writeString(obj.gender);
+    writer.writeString(obj.birthdate);
+    writer.writeString(obj.image);
+  }
+}
