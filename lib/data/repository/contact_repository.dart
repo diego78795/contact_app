3b4b3b2b8970b0e3788541cc8c0015e87a06b629
Diego Camargo <diego.cruz.camargo@gmail.com>
2023-07-17 17:12:15 -0300

    feat: Encaminhando funções de iniciar storage, buscar, editar e deletar contato

--- lib/data/repository/contact_repository.dart
+++ lib/data/repository/contact_repository.dart
@@ -9,11 +9,27 @@ class ContactRepository {
   ContactRepository({@required this.storageClient})
       : assert(storageClient != null);
 
+  Future initStorage() {
+    return storageClient!.initStorage();
+  }
+
   Future getContacts() {
     return storageClient!.storageGetContacts();
   }
 
   Future addContact(ContactModel contact) {
     return storageClient!.storageAddContact(contact);
+  }
+
+  Future getSingleContact(String key) {
+    return storageClient!.storageGetSingleContact(key);
+  }
+
+  Future editContact(String key, ContactModel contact) {
+    return storageClient!.storageEditContact(key, contact);
+  }
+
+  Future deleteContact(String key) {
+    return storageClient!.storageDeleteContact(key);
   }
 }
