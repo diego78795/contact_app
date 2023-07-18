8c246599594a10856b9ef6ce345902257017cf17
Diego Camargo <diego.cruz.camargo@gmail.com>
2023-07-18 12:36:46 -0300

    feat: Implementando modificações na contact e home binding

--- lib/bindings/contact_binding.dart
+++ lib/bindings/contact_binding.dart
@@ -1,14 +1,26 @@
 import 'package:get/get.dart';
 
-import 'package:contact_app/data/providers/storage.dart';
+import 'package:contact_app/external/datasource/storage.dart';
 import 'package:contact_app/controller/contact_controller.dart';
-import 'package:contact_app/data/repository/contact_repository.dart';
+import 'package:contact_app/infra/repository/contact_repository.dart';
+
+import 'package:contact_app/domain/usecase/edit_contact.dart';
+import 'package:contact_app/domain/usecase/init_storage.dart';
+import 'package:contact_app/domain/usecase/delete_contact.dart';
+import 'package:contact_app/domain/usecase/get_single_contact.dart';
 
 class ContactBinding implements Bindings {
   @override
   void dependencies() {
     Get.put(ContactController(
-      contactRepository: ContactRepository(storageClient: StorageClient()),
+      initStorageCase:
+          InitStorage(ContactRepository(storageClient: StorageClient())),
+      getSingleContactCase:
+          GetSingleContact(ContactRepository(storageClient: StorageClient())),
+      editContactCase:
+          EditContact(ContactRepository(storageClient: StorageClient())),
+      deleteContactCase:
+          DeleteContact(ContactRepository(storageClient: StorageClient())),
     ));
   }
 }
--- lib/bindings/home_binding.dart
+++ lib/bindings/home_binding.dart
@@ -1,14 +1,23 @@
 import 'package:get/get.dart';
 
-import 'package:contact_app/data/providers/storage.dart';
 import 'package:contact_app/controller/home_controller.dart';
-import 'package:contact_app/data/repository/contact_repository.dart';
+import 'package:contact_app/external/datasource/storage.dart';
+import 'package:contact_app/infra/repository/contact_repository.dart';
+
+import 'package:contact_app/domain/usecase/add_contact.dart';
+import 'package:contact_app/domain/usecase/init_storage.dart';
+import 'package:contact_app/domain/usecase/get_all_contacts.dart';
 
 class HomeBinding implements Bindings {
   @override
   void dependencies() {
     Get.put(HomeController(
-      contactRepository: ContactRepository(storageClient: StorageClient()),
+      initStorageCase:
+          InitStorage(ContactRepository(storageClient: StorageClient())),
+      addContactCase:
+          AddContact(ContactRepository(storageClient: StorageClient())),
+      getAllContactsCase:
+          GetAllContacts(ContactRepository(storageClient: StorageClient())),
     ));
   }
 }
