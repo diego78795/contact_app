eb0e8de4ce4d351fafbbe5729eeae0522b76db0e
Diego Camargo <diego.cruz.camargo@gmail.com>
2023-07-18 12:31:48 -0300

    feat: Movendo o Contact Model para a pasta domain

---
+++ lib/domain/model/contact_model.dart
@@ -0,0 +1,18 @@
+class ContactModel {
+  String name = '';
+  String nickname = '';
+  String email = '';
+  String telephone = '';
+  String gender = '';
+  String birthdate = '';
+  String image = '';
+
+  ContactModel(
+      {required name,
+      required nickname,
+      required email,
+      required telephone,
+      required gender,
+      required birthdate,
+      required image});
+}
