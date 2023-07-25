bacc67db11439b730ef3c5bf20e00ff32d6215fd
Diego Camargo <diego.cruz.camargo@gmail.com>
2023-07-25 08:35:20 -0300

    feat: Adaptando as novas modificações de usecase e repository na Home Binding

--- lib/bindings/home_binding.dart
+++ lib/bindings/home_binding.dart
@@ -1,16 +1,28 @@
-import 'package:contact_app/domain/usecase/contact_cases.dart';
 import 'package:get/get.dart';
 
 import 'package:contact_app/presenter/home/home_controller.dart';
 import 'package:contact_app/external/datasource/storage.dart';
-import 'package:contact_app/infra/repository/contact_repository.dart';
+
+import 'package:contact_app/infra/datasource/storage_abs.dart';
+import 'package:contact_app/infra/repository/add_contact_repository.dart';
+import 'package:contact_app/infra/repository/get_contact_repository.dart';
+import 'package:contact_app/infra/repository/init_storage_repository.dart';
+
+import 'package:contact_app/domain/usecase/add_contact_use_case.dart';
+import 'package:contact_app/domain/usecase/init_storage_use_case.dart';
+import 'package:contact_app/domain/usecase/get_contacts_use_case.dart';
 
 class HomeBinding implements Bindings {
   @override
   void dependencies() {
+    Get.lazyPut(() => StorageClient());
     Get.put(HomeController(
-      contactCases:
-          ContactCases(ContactRepository(storageClient: StorageClient())),
+      initStorageUseCase: InitStorageUseCase(
+          InitStorageRepository(storageClient: Get.find<StorageClientAbs>())),
+      getContactsUseCase: GetContactsUseCase(
+          GetContactsRepository(storageClient: Get.find<StorageClientAbs>())),
+      addContactUseCase: AddContactUseCase(
+          AddContactRepository(storageClient: Get.find<StorageClientAbs>())),
     ));
   }
 }
