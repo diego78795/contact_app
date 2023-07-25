39225755ba47b1e09b93f516ecdde5d150c3136e
Diego Camargo <diego.cruz.camargo@gmail.com>
2023-07-25 08:35:33 -0300

    feat: Adaptando as novas modificações de usecase e repository na Contact Binding

--- lib/bindings/contact_binding.dart
+++ lib/bindings/contact_binding.dart
@@ -2,16 +2,32 @@ import 'package:get/get.dart';
 
 import 'package:contact_app/external/datasource/storage.dart';
 import 'package:contact_app/presenter/contact/contact_controller.dart';
-import 'package:contact_app/infra/repository/contact_repository.dart';
 
-import 'package:contact_app/domain/usecase/contact_cases.dart';
+import 'package:contact_app/infra/datasource/storage_abs.dart';
+import 'package:contact_app/infra/repository/init_storage_repository.dart';
+import 'package:contact_app/infra/repository/delete_contact_repository.dart';
+import 'package:contact_app/infra/repository/contact_repository%20copy%204.dart';
+import 'package:contact_app/infra/repository/get_single_contact_repository.dart';
+
+import 'package:contact_app/domain/usecase/edit_contact_use_case.dart';
+import 'package:contact_app/domain/usecase/init_storage_use_case.dart';
+import 'package:contact_app/domain/usecase/delete_contact_use_case.dart';
+import 'package:contact_app/domain/usecase/get_single_contact_use_case.dart';
 
 class ContactBinding implements Bindings {
   @override
   void dependencies() {
+    Get.lazyPut(() => StorageClient());
     Get.put(ContactController(
-      contactCases:
-          ContactCases(ContactRepository(storageClient: StorageClient())),
+      initStorageUseCase: InitStorageUseCase(
+          InitStorageRepository(storageClient: Get.find<StorageClientAbs>())),
+      getSingleContactUseCases: GetSingleContactUseCase(
+          GetSingleContactRepository(
+              storageClient: Get.find<StorageClientAbs>())),
+      editContactUseCases: EditContactUseCase(
+          EditContactRepository(storageClient: Get.find<StorageClientAbs>())),
+      deleteContactUseCases: DeleteContactUseCase(
+          DeleteContactRepository(storageClient: Get.find<StorageClientAbs>())),
     ));
   }
 }
